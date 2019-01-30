90002cc6b2ec206916f2f7b622b77e7dbc8ca320
rafamolino <35599091+rafamolino@users.noreply.github.com>
2019-01-30 10:52:49 +0100

    Funcionamiento pantalla "Personas" y Pantalla Bloc de Notas en proceso

--- app/src/main/java/com/example/rafa/practica6pmdm/BlocDeNotas.java
+++ app/src/main/java/com/example/rafa/practica6pmdm/BlocDeNotas.java
@@ -2,6 +2,8 @@ package com.example.rafa.practica6pmdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class BlocDeNotas extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class BlocDeNotas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bloc_de_notas);
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mnu_blocnotas, menu);
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/rafa/practica6pmdm/Personas.java
+++ app/src/main/java/com/example/rafa/practica6pmdm/Personas.java
@@ -2,12 +2,42 @@ package com.example.rafa.practica6pmdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Personas extends AppCompatActivity {
+    ImageView imgCasillas;
+    ImageView imgFabregas;
+    ImageView imgIniesta;
+    ImageView imgPuyol;
+    ImageView imgVilla;
+    ImageView imgXabi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personas);
+
+        imgCasillas = findViewById(R.id.imgCasillas);
+        imgFabregas = findViewById(R.id.imgFabregas);
+        imgIniesta = findViewById(R.id.imgIniesta);
+        imgPuyol = findViewById(R.id.imgPuyol);
+        imgVilla = findViewById(R.id.imgVilla);
+        imgXabi = findViewById(R.id.imgXabi);
+
+        registerForContextMenu(imgCasillas);
+        registerForContextMenu(imgFabregas);
+        registerForContextMenu(imgIniesta);
+        registerForContextMenu(imgPuyol);
+        registerForContextMenu(imgVilla);
+        registerForContextMenu(imgXabi);
+
+    }
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mnu_personas, menu);
     }
 }
