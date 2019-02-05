ef166626d5a8d464b17554fc9fe06056c65e7bca
rafamolino <molinorafael99@gmail.com>
2019-02-05 23:51:10 +0100

    Pantalla "Personas": Sin acabar

--- app/src/main/java/com/example/rafa/practica6pmdm/Personas.java
+++ app/src/main/java/com/example/rafa/practica6pmdm/Personas.java
@@ -1,9 +1,14 @@
 package com.example.rafa.practica6pmdm;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -39,5 +44,24 @@ public class Personas extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.mnu_personas, menu);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mnu_editarcontactos, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.editarContacto:
+
+                Intent i1 = new Intent(this, EditarContactos.class);
+                startActivity(i1);
+
+                break;
+
+
+
+        }
+        return true;
     }
 }
