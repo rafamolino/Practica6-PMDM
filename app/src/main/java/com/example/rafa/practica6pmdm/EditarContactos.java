ddaa5ae36b0f9143677c599e24c2fabfeb0c9635
rafamolino <molinorafael99@gmail.com>
2019-02-06 22:18:18 +0100

    Pantalla "Personas": Acabada

--- app/src/main/java/com/example/rafa/practica6pmdm/EditarContactos.java
+++ app/src/main/java/com/example/rafa/practica6pmdm/EditarContactos.java
@@ -1,13 +1,113 @@
 package com.example.rafa.practica6pmdm;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class EditarContactos extends AppCompatActivity {
 
+    ImageView imgCasillas;
+    ImageView imgFabregas;
+    ImageView imgIniesta;
+    ImageView imgPuyol;
+    ImageView imgVilla;
+    ImageView imgXabi;
+    String numeroTelefono;
+    String correoElectronico;
+    EditText txtTelefono;
+    EditText txtCorreo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_contactos);
+
+        imgCasillas = findViewById(R.id.imgCasillas);
+        imgFabregas = findViewById(R.id.imgFabregas);
+        imgIniesta = findViewById(R.id.imgIniesta);
+        imgPuyol = findViewById(R.id.imgPuyol);
+        imgVilla = findViewById(R.id.imgVilla);
+        imgXabi = findViewById(R.id.imgXabi);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreo = findViewById(R.id.txtCorreo);
+    }
+
+    public void editCasillas(View view) {
+        imgCasillas.setBackgroundColor(getResources().getColor(R.color.backgroundColor));
+        imgPuyol.setBackgroundColor(android.R.drawable.btn_default);
+        imgFabregas.setBackgroundColor(android.R.drawable.btn_default);
+        imgVilla.setBackgroundColor(android.R.drawable.btn_default);
+        imgIniesta.setBackgroundColor(android.R.drawable.btn_default);
+        imgXabi.setBackgroundColor(android.R.drawable.btn_default);
+        numeroTelefono="numeroTelefono1";
+        correoElectronico="correoElectronico1";
+    }
+
+    public void editPuyol(View view) {
+        imgPuyol.setBackgroundColor(getResources().getColor(R.color.backgroundColor));
+        imgCasillas.setBackgroundColor(android.R.drawable.btn_default);
+        imgFabregas.setBackgroundColor(android.R.drawable.btn_default);
+        imgVilla.setBackgroundColor(android.R.drawable.btn_default);
+        imgIniesta.setBackgroundColor(android.R.drawable.btn_default);
+        imgXabi.setBackgroundColor(android.R.drawable.btn_default);
+        numeroTelefono="numeroTelefono2";
+        correoElectronico="correoElectronico2";
+    }
+
+    public void editFabregas(View view) {
+        imgFabregas.setBackgroundColor(getResources().getColor(R.color.backgroundColor));
+        imgPuyol.setBackgroundColor(android.R.drawable.btn_default);
+        imgCasillas.setBackgroundColor(android.R.drawable.btn_default);
+        imgVilla.setBackgroundColor(android.R.drawable.btn_default);
+        imgIniesta.setBackgroundColor(android.R.drawable.btn_default);
+        imgXabi.setBackgroundColor(android.R.drawable.btn_default);
+        numeroTelefono="numeroTelefono3";
+        correoElectronico="correoElectronico3";
+    }
+
+    public void editVilla(View view) {
+        imgVilla.setBackgroundColor(getResources().getColor(R.color.backgroundColor));
+        imgPuyol.setBackgroundColor(android.R.drawable.btn_default);
+        imgFabregas.setBackgroundColor(android.R.drawable.btn_default);
+        imgCasillas.setBackgroundColor(android.R.drawable.btn_default);
+        imgIniesta.setBackgroundColor(android.R.drawable.btn_default);
+        imgXabi.setBackgroundColor(android.R.drawable.btn_default);
+        numeroTelefono="numeroTelefono4";
+        correoElectronico="correoElectronico4";
+    }
+
+    public void editIniesta(View view) {
+        imgIniesta.setBackgroundColor(getResources().getColor(R.color.backgroundColor));
+        imgPuyol.setBackgroundColor(android.R.drawable.btn_default);
+        imgFabregas.setBackgroundColor(android.R.drawable.btn_default);
+        imgVilla.setBackgroundColor(android.R.drawable.btn_default);
+        imgCasillas.setBackgroundColor(android.R.drawable.btn_default);
+        imgXabi.setBackgroundColor(android.R.drawable.btn_default);
+        numeroTelefono="numeroTelefono5";
+        correoElectronico="correoElectronico5";
+    }
+
+    public void editXabiAlonso(View view) {
+        imgXabi.setBackgroundColor(getResources().getColor(R.color.backgroundColor));
+        imgPuyol.setBackgroundColor(android.R.drawable.btn_default);
+        imgFabregas.setBackgroundColor(android.R.drawable.btn_default);
+        imgVilla.setBackgroundColor(android.R.drawable.btn_default);
+        imgIniesta.setBackgroundColor(android.R.drawable.btn_default);
+        imgCasillas.setBackgroundColor(android.R.drawable.btn_default);
+        numeroTelefono="numeroTelefono6";
+        correoElectronico="correoElectronico6";
+    }
+
+    public void onClick(View view) {
+        SharedPreferences prefs = getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(numeroTelefono, txtTelefono.getText().toString());
+        editor.putString(correoElectronico, txtCorreo.getText().toString());
+        editor.commit();
+        Toast.makeText(this,getString(R.string.edicionContactos) , Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/rafa/practica6pmdm/Personas.java
+++ app/src/main/java/com/example/rafa/practica6pmdm/Personas.java
@@ -1,25 +1,40 @@
 package com.example.rafa.practica6pmdm;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.Manifest.permission.CALL_PHONE;
 
 public class Personas extends AppCompatActivity {
+    SharedPreferences prefs;
     ImageView imgCasillas;
     ImageView imgFabregas;
     ImageView imgIniesta;
     ImageView imgPuyol;
     ImageView imgVilla;
     ImageView imgXabi;
+    String numeroTelefono;
+    String correoElectronico;
+    String str;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +54,55 @@ public class Personas extends AppCompatActivity {
         registerForContextMenu(imgVilla);
         registerForContextMenu(imgXabi);
 
+        imgCasillas.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                correoElectronico="correoElectronico1";
+                numeroTelefono="numeroTelefono1";
+                return false;
+            }
+        });
+        imgPuyol.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                correoElectronico="correoElectronico2";
+                numeroTelefono="numeroTelefono2";
+                return false;
+            }
+        });
+        imgFabregas.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                correoElectronico="correoElectronico3";
+                numeroTelefono="numeroTelefono3";
+                return false;
+            }
+        });
+        imgVilla.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                correoElectronico="correoElectronico4";
+                numeroTelefono="numeroTelefono4";
+                return false;
+            }
+        });
+        imgIniesta.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                correoElectronico="correoElectronico5";
+                numeroTelefono="numeroTelefono5";
+                return false;
+            }
+        });
+        imgXabi.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                correoElectronico="correoElectronico6";
+                numeroTelefono="numeroTelefono6";
+                return false;
+            }
+        });
+
     }
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
@@ -50,18 +114,67 @@ public class Personas extends AppCompatActivity {
         inflater.inflate(R.menu.mnu_editarcontactos, menu);
         return true;
     }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        prefs = getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+
+        switch (item.getItemId())
+        {
+
+            case R.id.llamar:
+            str = prefs.getString(numeroTelefono, null);
+            if(str==null)
+            {
+                Toast.makeText(this, getString(R.string.errorTelefono),  Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
+                {
+
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:" + str));
+                    startActivity(callIntent);
+                }
+                else
+                {
+                    requestPermissions(new String[]{CALL_PHONE}, 1);
+                }
+            }
+            break;
+            case R.id.correo:
+            str = prefs.getString(correoElectronico, null);
+            if(str==null)
+            {
+                Toast.makeText(this, getString(R.string.errorCorreo), Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("text/plain");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{str});
+                startActivity(emailIntent);
+            }
+            break;
+
+
+
+    }
+        return super.onContextItemSelected(item);
+
+    }
+
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.editarContacto:
 
                 Intent i1 = new Intent(this, EditarContactos.class);
                 startActivity(i1);
-
                 break;
-
-
-
         }
         return true;
     }
+
+
 }
